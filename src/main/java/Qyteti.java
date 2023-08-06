a4c0f468205ddc09eaa19cd8f42d0dfc77f52971
Gledi <gledi613@gmail.com>
2023-08-06 10:14:12 +0200

    a

--- src/main/java/Qyteti.java
+++ src/main/java/Qyteti.java
@@ -1,2 +1,13 @@
+import jakarta.persistence.*;
+
+@Entity(name = "Qyteti")
 public class Qyteti {
+@Id
+@GeneratedValue(strategy = GenerationType.SEQUENCE )
+private  int qyteti_id;
+@OneToMany(mappedBy = "qyteti_id")
+   private Moti moti;
+
+
+
 }
