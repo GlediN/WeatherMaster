0392f9db2f36c2f6d96c50f2ecc194fb60a1f28c
Kursi <arbri,gjylbegaj@gmail.com>
2023-08-06 14:31:13 +0200

    upd

---
+++ src/main/java/entity/WeatherServiceImplementations.java
@@ -0,0 +1,13 @@
+package entity;
+
+public class WeatherServiceImplementations implements WeatherServiceInterface{
+    @Override
+    public void default_cities() {
+
+    }
+
+    @Override
+    public void weather_by_city() {
+
+    }
+}
---
+++ src/main/java/entity/WeatherServiceInterface.java
@@ -0,0 +1,9 @@
+package entity;
+
+public interface WeatherServiceInterface {
+    void default_cities();
+
+    void weather_by_city();
+
+
+}
